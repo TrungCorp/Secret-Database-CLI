17833874309ed040022bdbd518f37abba5a666bc
Trung Le <trungle9995@gmail.com>
2024-12-07 15:03:04 -0500

    modified code

--- lib/cli.py
+++ lib/cli.py
@@ -3,7 +3,6 @@
 #IMPORT HELP FUNCTIONS HERE
 from helpers import (
     exit_program,
-    check_homerooms,
     convert_room_to_id,
     list_homerooms,
     list_students_in_hr,
@@ -12,25 +11,30 @@ from helpers import (
     delete_Homeroom,
     create_homeroom,
     list_by_floor,
-    int_check
+    int_check,
+    create_student
 )
 
 
 def main():
+    #LVL 1
     choice = ""
+    #LVL 2
     inner_choice = ""
+    #LVL 3
+    super_inner_choice = ""
     print("Welcome Counselor")
     menu_top()
     
     seed_data()
     while choice !="q":
-        #CHOICE LEVEL 1
+        #RESETS INNER_CHOICE AND SUPER_INNER
         inner_choice = ""
+        super_inner_choice = ""
         
         menu()
-
-        choice = input("> ")
-
+        choice = valid_choice("> ")
+        
         if choice == "q":
             exit_program()
         elif choice == "e":
@@ -53,10 +57,15 @@ def main():
                 elif inner_choice =='t':
                     t_option = int_check("Enter floor level: ")
                     list_by_floor(t_option)
-
+                elif inner_choice == 'e':
+                    print("e choice selected")
                 #checks if room # is found in Homeroom table
-                elif check_homerooms((inner_choice)):
-                    list_students_in_hr(convert_room_to_id(inner_choice))
+                elif (inner_choice) =='e':
+                    e_option = int_check("Enter the room number of homeroom: ")
+                    list_students_in_hr(convert_room_to_id(e_option))
+                    super_inner_choice = input("> ")
+                    while super_inner_choice !='b':
+                        super_inner_menu()
                 
                     
                 
@@ -75,12 +84,19 @@ def inner_menu():
     
     print("b. Go back")
     print("q. Exit the program")
+    print("g. View all Homerooms")
+    print("f. View Homeroom by floor level")
     print("a. Add a homeroom")
-    print("t. List by floor")
+    
     print("r. Remove a homeroom")
 
 def super_inner_menu():
-    pass
+    menu_top()
+    print("b. Go back")
+    print("q. Exit the program")
+    print("a. Add a student")
+    print("r. Remove a student")
+    
 
 
 if __name__ == "__main__":
--- lib/helpers.py
+++ lib/helpers.py
@@ -10,8 +10,12 @@ def seed_data():
     Student.drop_table()
     Student.create_table()
     homeroom_101 = Homeroom.create(101,"Mr. Taylor",1)
-    homeroom_201 = Homeroom.create(201,"Ms. Martha",2)
     homeroom_103 = Homeroom.create(103,"Mr Jackson",1)
+    homeroom_201 = Homeroom.create(201,"Ms. Martha",2)
+    homeroom_301 = Homeroom.create(301,"Mrs. Casca",3)
+    
+    Student.create("JoJo Joestar",10,2.7,0,homeroom_301)
+    Student.create("Josuke Joestar",9,3.7,2,homeroom_301)
     Student.create("Alexander Jono",9,4.0,2,homeroom_103)
     Student.create("Cynthia Unos",10,3.8,2,homeroom_201)
     Student.create("Mark Unos",12,4.0,2,homeroom_201)
@@ -22,6 +26,7 @@ def seed_data():
 def delete_Homeroom():
     hr_room = input("Enter the room number of homeroom you want to remove: ")
     if homeroom := Homeroom.find_by_room(hr_room):
+        #DELETES HOMEROOM
         homeroom.delete()
         print(f"Homeroom {hr_room} deleted")
     else:
@@ -39,34 +44,24 @@ def list_by_floor(hr_floor):
         print("Floor does not exist")
         
 
-def check_homerooms(target_room):
-    if (isinstance(target_room,str)):
-        return None
-    homerooms = Homeroom.get_all()
-    for homeroom in homerooms:
-        if homeroom.room == target_room:
-            #print(f"Found homeroom: {homeroom}")
-            return homeroom
-    
-    print(f"No homeroom found with room number {target_room}")
-        
-    return None
+
 
 def list_homerooms():
     homerooms = Homeroom.get_all()
     for homeroom in homerooms:
         print(homeroom)
-
+#VALIDATES USER INPUT
 def valid_choice(prompt):
     user_input = input(prompt)
     if user_input.lower() in ['q','a','b','e','r','t']:
         return user_input.lower()
     try:
         int(user_input)
-        return int(user_input)
+        return int(user_input)                                                                                                                                                                                                                                                                                                                                                                                                                                
     except ValueError:
         print("invalid input #4")
-        return user_input
+        return user_input.lower()
+    
 def int_check(prompt):
     user_input = input(prompt)
     try:
@@ -75,13 +70,14 @@ def int_check(prompt):
     except ValueError:
         print("invalid input #5")
         return user_input
+    
 def convert_room_to_id(num):
     homerooms = Homeroom.get_all()
     for homeroom in homerooms:
         if num == homeroom.room:
-            print(f"TEST: HOMEROOM ID: {homeroom.id}")
             return homeroom.id
     return None
+
 def create_homeroom():
     room = input("Enter the homeroom room number: ")
     teacher = input("Enter name of the teacher: ")
@@ -92,7 +88,17 @@ def create_homeroom():
     except Exception:
         print("Error: ",Exception)
 
-    
+def create_student():
+    name = input("Enter the name of the student: ")
+    grade = input("Enter the grade the student is in: ")
+    gpa = input("Enter the students GPA: ")
+    honors_classes = input("Enter the number of honors classes the student is in: ")
+    try:
+        student = Student.create(name,int(grade),float(gpa),int(honors_classes))
+        print(f'Success {student}')
+    except Exception as exec:
+        print("Error: ",exec)
+
 def list_students_in_hr(num):
     
     students = Student.get_all()
